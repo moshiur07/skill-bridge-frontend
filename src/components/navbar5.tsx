ecd1b7fe27158f5034fc0fbafd8d3d22014336b1
MD. Moshiur Rahman <rifatmoshiur00@gmail.com>
2026-02-10 13:36:55 +0600

    navbar responsive design added

--- src/components/navbar5.tsx
+++ src/components/navbar5.tsx
@@ -30,13 +30,11 @@ interface Navbar5Props {
 
 const Navbar5 = ({ className }: Navbar5Props) => {
   return (
-    <section className={cn(" relative", className)}>
-      <div className="absolute z-10  w-full py-4">
+    <section className={cn("relative", className)}>
+      <div className=" z-10  w-full py-4 fixed top-0 left-0   bg-opacity-50 backdrop-blur-lg  transition-all duration-300 ease-in-out bg-white/0">
+        {/* <div className="absolute z-10  w-full py-4 "> */}
         <nav className="flex justify-between px-10 ">
-          <Link
-            href="/"
-            className="flex items-center gap-2 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:scale-x-100"
-          >
+          <Link href="/" className="flex items-center gap-2">
             <Image
               src="/skillbridge logo.png"
               className="max-h-8"
@@ -85,44 +83,59 @@ const Navbar5 = ({ className }: Navbar5Props) => {
             </Link>
           </div>
           <Sheet>
-            <SheetTrigger asChild className="lg:hidden">
+            <SheetTrigger asChild className="lg:hidden bg-transparent">
               <Button variant="outline" size="icon">
                 <MenuIcon className="h-4 w-4" />
               </Button>
             </SheetTrigger>
-            <SheetContent side="top" className="max-h-screen overflow-auto">
+            <SheetContent
+              side="top"
+              className="max-h-screen flex  rounded-xl w-1/2 overflow-auto backdrop-blur-sm bg-white/30 border border-white/20 text-white"
+            >
               <SheetHeader>
                 <SheetTitle>
-                  <a
-                    href="https://www.shadcnblocks.com"
-                    className="flex items-center gap-2"
-                  >
-                    <img
-                      src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-icon.svg"
+                  <Link href="/" className="flex items-center gap-2">
+                    <Image
+                      src="/skillbridge logo.png"
                       className="max-h-8"
+                      height={32}
+                      width={32}
                       alt="logo"
                     />
-                    <span className="text-lg font-semibold tracking-tighter">
-                      SkillBridge
+                    <span className="text-lg font-semibold text-white hover:text-black">
+                      Skill Bridge
                     </span>
-                  </a>
+                  </Link>
                 </SheetTitle>
               </SheetHeader>
               <div className="flex flex-col p-4">
                 <div className="flex flex-col gap-6">
-                  <a href="#" className="font-medium">
-                    Templates
-                  </a>
-                  <a href="#" className="font-medium">
-                    Blog
-                  </a>
-                  <a href="#" className="font-medium">
-                    Pricing
-                  </a>
+                  <Link
+                    href={"/tutors"}
+                    className="flex items-center gap-2 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:scale-x-100"
+                  >
+                    Tutors
+                  </Link>
+                  <Link
+                    href={"/about"}
+                    className="flex items-center gap-2 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:scale-x-100"
+                  >
+                    About
+                  </Link>
+                  <Link
+                    href={"/dashboard"}
+                    className="flex items-center gap-2 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:scale-x-100"
+                  >
+                    Dashboard
+                  </Link>
                 </div>
                 <div className="mt-6 flex flex-col gap-4">
-                  <Button variant="outline">Sign in</Button>
-                  <Button>Start for free</Button>
+                  <Link
+                    href={"/login"}
+                    className="bg-transparent text-xl hover:bg-transparent hover:text-black relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:scale-x-100"
+                  >
+                    Log in
+                  </Link>
                 </div>
               </div>
             </SheetContent>
