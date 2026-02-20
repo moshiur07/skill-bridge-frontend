2595c2b4a9fcb513de114bdf82c0edbb87f0f912
MD. Moshiur Rahman <rifatmoshiur00@gmail.com>
2026-02-20 22:54:44 +0600

    signup and with proper validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
@@ -18,8 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={ubuntu.className}>
+    <html lang="en" suppressHydrationWarning className={ubuntu.className}>
       <body>{children}</body>
+      <Toaster richColors />
     </html>
   );
 }
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -1,9 +1,12 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Field,
+  FieldContent,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
@@ -19,16 +22,74 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+import { useForm } from "@tanstack/react-form";
+
+import * as z from "zod";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+
+const formSchema = z.object({
+  name: z.string().min(4, "This Field is Required!"),
+  password: z.string().min(6, "Password should 6 character or above!"),
+  email: z.string().min(6, "Please enter a valid email!"),
+  role: z.string(),
+});
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const signUpWithEmail = () => {
+    console.log("got clicked!");
+  };
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      role: "student",
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value);
+      const toastID = toast.loading("Creating User...", {
+        position: "top-center",
+      });
+      try {
+        const { data, error } = await authClient.signUp.email(value);
+        if (error) {
+          toast.error(error.message, { id: toastID, position: "top-center" });
+        }
+        if (data) {
+          toast.success(`${value.name}, Welcome to skillBridge!`, {
+            id: toastID,
+            position: "top-center",
+            duration: 5000,
+          });
+        }
+      } catch (error: any) {
+        toast.error("Internal Server error...", {
+          id: toastID,
+          position: "top-center",
+          description: `${error.message}`,
+        });
+      }
+    },
+  });
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form
+            className="p-6 md:p-8"
+            onSubmit={(e) => {
+              e.preventDefault();
+              form.handleSubmit();
+            }}
+          >
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Create your account</h1>
@@ -37,62 +98,123 @@ export function SignupForm({
                 </p>
               </div>
               {/* name */}
-              <Field>
-                <FieldLabel htmlFor="name">Name</FieldLabel>
-                <Input
-                  id="name"
-                  type="text"
-                  placeholder="Your Name"
-                  required
-                  className="bg-slate-50"
-                />
-              </Field>
+              <form.Field
+                name="name"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field>
+                      <FieldLabel htmlFor="name">Name</FieldLabel>
+                      <Input
+                        id="name"
+                        type="text"
+                        placeholder="Your Name"
+                        required
+                        className="bg-slate-50"
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
               {/* email */}
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                  className="bg-slate-50"
-                />
-              </Field>
+              <form.Field
+                name="email"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field>
+                      <FieldLabel htmlFor="email">Email</FieldLabel>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                        className="bg-slate-50"
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+
               {/* password */}
-              <Field>
-                <Field className="grid grid-cols-2 gap-4">
-                  <Field>
-                    <FieldLabel htmlFor="password">Password</FieldLabel>
-                    <Input
-                      id="password"
-                      type="password"
-                      placeholder="password"
-                      required
-                      className="bg-slate-50"
-                    />
-                  </Field>
-                </Field>
-              </Field>
+              <form.Field
+                name="password"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field>
+                      <FieldLabel htmlFor="password">Password</FieldLabel>
+                      <Input
+                        id="password"
+                        type="password"
+                        placeholder="password"
+                        required
+                        className="bg-slate-50"
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+
               {/* role */}
-              <Field>
-                <Field className="grid grid-cols-2 gap-4">
-                  <Field>
-                    <FieldLabel htmlFor="role">Role</FieldLabel>
-                    <Select required>
-                      <SelectTrigger className="bg-slate-50">
-                        <SelectValue placeholder="Select your Role" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>Role</SelectLabel>
+              <form.Field
+                name="role"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field
+                      aria-required
+                      className="w-1/2"
+                      orientation="responsive"
+                    >
+                      <FieldContent>
+                        <FieldLabel htmlFor="form-tanstack-select-language">
+                          Role
+                        </FieldLabel>
+                      </FieldContent>
+                      <Select
+                        required
+                        name={field.name}
+                        value={field.state.value}
+                        onValueChange={field.handleChange}
+                      >
+                        <SelectTrigger
+                          id="form-tanstack-select-language"
+                          className="bg-slate-50"
+                        >
+                          <SelectValue placeholder="Select your role" />
+                        </SelectTrigger>
+                        <SelectContent position="item-aligned">
                           <SelectItem value="student">Student</SelectItem>
                           <SelectItem value="tutor">Tutor</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  </Field>
-                </Field>
-              </Field>
+                        </SelectContent>
+                      </Select>
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
               <Field>
                 <Button type="submit">Create Account</Button>
               </Field>
